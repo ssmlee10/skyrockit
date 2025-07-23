09b0dd9b856685c34568d95d6e9400dc42ed3b64
Sharon Lee <ssmlee10@gmail.com>
2025-07-23 12:08:19 -0700

    build the application controller skeleton

---
+++ controllers/applications.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+// router logic here
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 // imports logic from auth.js in controllers
 const authController = require("./controllers/auth.js");
 
+// applications controller
+const applicationsController = require('./controllers/applications.js');
+
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -45,6 +48,7 @@ app.get("/", async (req, res) => {
 
 app.use("/auth", authController); // these are the auth routes
 app.use(isSignedIn); // middleware
+app.use('/users/:userId/applications', applicationsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
