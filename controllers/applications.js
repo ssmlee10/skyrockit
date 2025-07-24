8c7ad3a43af1081229f8d27fa99d4b44d10c0abd
Sharon Lee <ssmlee10@gmail.com>
2025-07-24 11:54:53 -0700

    show data on our applications index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const User = require("../models/user.js");
 
 // router logic here
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.render("applications/index.ejs");
+    const currentUser = await User.findById(req.session.user._id);
+    res.render("applications/index.ejs", {
+      applications : currentUser.applications
+    });
   } catch (error) {
     console.log(error);
     res.redirect('/');
