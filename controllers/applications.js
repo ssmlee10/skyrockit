e15b5dd42e0bf58eb7d7da090f517714a05199cb
Sharon Lee <ssmlee10@gmail.com>
2025-07-24 10:29:18 -0700

    complete new application functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -18,4 +18,25 @@ router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
 });
 
+// POST for '/applications
+router.post('/', async (req, res) => {
+  try {
+    // look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+
+    // push req.body (the new data form object) to the applications array of the current user
+    currentUser.applications.push(req.body);
+
+    // save changes to the user
+    await currentUser.save();
+
+    //redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+})
+
 module.exports = router;
